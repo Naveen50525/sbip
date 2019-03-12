e4d7633f9146a911f244476f382e4d328d8e6ea5
Naveen50525 <37217362+Naveen50525@users.noreply.github.com>
2019-03-12 16:52:17 +0530

    Update ball_speed.py

--- src/ball_speed.py
+++ src/ball_speed.py
@@ -1,12 +1,9 @@
 import rospy
 import cv2
 import numpy as np
-import time
 import rospy
 from geometry_msgs.msg import Twist 
 from geometry_msgs.msg import Pose
-
-
 
 class ball_speed():
     def __init__(self):
@@ -22,7 +19,6 @@ class ball_speed():
         self.dif_y = -((self.pos_old_y - self.pos_y))
         self.pos_old_x = self.pos_x
         self.pos_old_y = self.pos_y
-        #publish speed_x and speed._y
     
 if __name__ == '__main__':
     try:
@@ -31,8 +27,8 @@ if __name__ == '__main__':
         bs = ball_speed()
         tw = Twist()
         while(True):
-            tw.linear.x = bs.difx*60
-            tw.linear.y = bs.dify*60
+            tw.linear.x = bs.dif_x*60
+            tw.linear.y = bs.dif_y*60
             rate.sleep()
 
     except rospy.ROSInterruptException: pass
